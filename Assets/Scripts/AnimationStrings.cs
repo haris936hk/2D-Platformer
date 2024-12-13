bf5d20977a9caa0fa18f6c63d8122ea0f858e7f3
haris936hk <hariskhan936.hk@gmail.com>
2024-12-14 04:07:55 +0500

    Initial commit

---
+++ Assets/Scripts/AnimationStrings.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class AnimationStrings{
+    internal static string isMoving = "isMoving";
+    internal static string isRunning = "isRunning";
+    internal static string isGrounded = "isGrounded";
+    internal static string yVelocity = "yVelocity";
+}
